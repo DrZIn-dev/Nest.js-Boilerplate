9380f5e5412dc83974df6ee866c0498885658cf1
pasawee laearun <pasawee.dev@gmail.com>
2021-11-20 20:11:39 +0700

    Chore: remove member controller

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -1,12 +1,11 @@
 import { Member } from '@/model/member.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MemberController } from './member.controller';
 import { MemberService } from './member.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Member])],
   providers: [MemberService],
-  controllers: [MemberController],
+  controllers: [],
 })
 export class MemberModule {}
