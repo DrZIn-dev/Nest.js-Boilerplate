1ccfd2724292380d09a4f7d01ca0efff29332ded
pasawee laearun <pasawee.dev@gmail.com>
2021-11-20 20:04:53 +0700

    Update: jest config

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    '.entity.ts',
+    'migration',
+    'scripts',
+    '.module.ts',
+    'main.ts',
+  ],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+};
