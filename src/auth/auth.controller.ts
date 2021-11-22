4d4d314f0400f0eeb4353f87d698206a3988a64b
pasawee laearun <pasawee.dev@gmail.com>
2021-11-22 12:45:49 +0700

    Update: import member

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,7 @@
+import { MemberService } from '@/member/member.service';
 import { Controller } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private memberService: MemberService) {}
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
+import { MemberService } from '@/member/member.service';
+import { MemberEntity } from '@/model/member.entity';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MemberEntity])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, MemberService],
 })
 export class AuthModule {}
