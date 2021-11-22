a036a5d4890586a992367dd4138e02769cc36a01
pasawee laearun <pasawee.dev@gmail.com>
2021-11-22 12:59:15 +0700

    Update: register member

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
+import { CreateMemberDto } from '@/member/member.dto';
 import { MemberService } from '@/member/member.service';
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
   constructor(private memberService: MemberService) {}
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  public async register(@Body() dto: CreateMemberDto) {
+    return await this.memberService.create(dto);
+  }
 }
