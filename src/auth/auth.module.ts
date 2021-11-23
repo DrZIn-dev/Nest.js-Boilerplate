538fb97cb33ccddd096d270069f2945bc3cf7f35
pasawee laearun <pasawee.dev@gmail.com>
2021-11-23 14:03:14 +0700

    Update: JWTModule Global

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,23 +1,14 @@
-import { configService } from '@/config/config.service';
 import { MemberService } from '@/member/member.service';
 import { MemberEntity } from '@/model/member.entity';
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([MemberEntity]),
-    JwtModule.register({
-      secret: configService.getJwtSecret(),
-      signOptions: { expiresIn: '7d' },
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([MemberEntity])],
   controllers: [AuthController],
-  providers: [AuthService, MemberService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, MemberService, LocalStrategy],
 })
 export class AuthModule {}
